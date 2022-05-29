aba0fbfad2bbcadea5b7e08f44798256c63e3c6f
danisal <daniel.salvado@gmail.com>
2022-05-29 17:37:13 +0100

    Load propert image format

--- src/lib/components/bio.svelte
+++ src/lib/components/bio.svelte
@@ -1,25 +1,30 @@
-<script>
-	import profilePic from '$lib/assets/profile-pic.jpg';
-</script>
-
 <br />
 <h3>About the Author</h3>
 <div class="flex gap-4">
-	<img
-		alt="Daniel Profile"
-		class="h-24 w-24 shrink-0 rounded-full object-cover object-center"
-		loading="lazy"
-		src={profilePic}
-	/>
-	<div>
-		<h4 class="mb-2.5">Daniel Salvado</h4>
-		<p>
-			Software Engineer | Developer | JavaScript Enthusiast. Strive to improve on code and the
-			people around me! Follow
-			{` `}
-			<a class="text-blue-800 no-underline" href={`https://twitter.com/@danisalTweets`}>
-				@danisalTweets
-			</a>
-		</p>
-	</div>
+    <picture class="shrink-0">
+        <source type="image/avif" srcset="assets/profile-pic.avif" />
+        <source type="image/webp" srcset="assets/profile-pic.webp" />
+        <img
+            alt="Daniel Salvado"
+            class="rounded-full h-24 w-24 aspect-square object-cover"
+            loading="lazy"
+            src="assets/profile-pic.jpg"
+            width="24"
+            height="24"
+        />
+    </picture>
+    <div>
+        <h4 class="mb-2.5">Daniel Salvado</h4>
+        <p>
+            Software Engineer | Developer | JavaScript Enthusiast. Strive to
+            improve on code and the people around me! Follow
+            {` `}
+            <a
+                class="text-blue-800 no-underline"
+                href="https://twitter.com/@danisalTweets"
+            >
+                @danisalTweets
+            </a>
+        </p>
+    </div>
 </div>
