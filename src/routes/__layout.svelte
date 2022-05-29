ba2170f649a5cbb4a1b79de164184d0f1ca629aa
danisal <daniel.salvado@gmail.com>
2022-05-29 16:28:33 +0100

    Add favicon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,9 @@
 <script>
-	import Footer from '$lib/components/footer.svelte';
-	import Header from '$lib/components/header.svelte';
+    import Footer from "$lib/components/footer.svelte";
+    import Header from "$lib/components/header.svelte";
 
-	import '../app.css';
+    import "../app.css";
 </script>
-
-<svelte:head>
-	<link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
-	<link
-		rel="stylesheet"
-		href="https://fonts.googleapis.com/css?family=Reenie+Beanie&display=swap&text=DanielSalvado"
-	/>
-</svelte:head>
 
 <Header />
 <slot />
