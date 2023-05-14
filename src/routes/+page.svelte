b0508d483601b99467922179624b97d844b5cef1
danisal <daniel.salvado@gmail.com>
2023-05-14 15:13:34 +0100

    Refactor home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,28 @@
-<script>
-    import Bio from "$lib/components/bio.svelte";
-</script>
-
 <svelte:head>
-    <title>Home | Daniel Salvado</title>
+	<title>Home | Daniel Salvado</title>
 </svelte:head>
 
-<main class="container prose mx-auto px-7 py-16 relative prose-zync">
-    <!-- <div
-        class="absolute top-0 -left-1/4 w-[500px] h-[500px] bg-teal-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"
-    />
-    <div
-        class="absolute top-1/4 -right-1/2 w-[500px] h-[500px] bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"
-    />
-    <div
-        class="absolute -bottom-1/4 left-1/4 w-[500px] h-[500px] bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"
-    /> -->
-
-    <h1>Welcome to my personal virtual space!</h1>
-    <p>
-        Hi, my name is Daniel Salvado, I'm an enthusiastic developer who loves
-        to work within the JavaScript ecosystem and always strive to improve on
-        code and build beautiful apps.
-    </p>
-    <p>
-        In here you will be able to find some of the work that I've done, places
-        where I've worked and some articles that I've written.
-    </p>
-    <p>
-        I love to share my know-how and things that I find interesting and
-        because of that I do some writing on things that I've tested and in some
-        way, helped me on the products that I was building!
-    </p>
-    <p>
-        Feel free to share the articles or even to contact me if you have any
-        questions, suggestions or simply say hi!
-    </p>
-    <Bio />
+<main class="prose-zync container prose relative mx-auto px-7 py-16">
+	<h1>Welcome to my personal virtual space!</h1>
+	<picture class="mx-auto">
+		<source type="image/avif" srcset="assets/profile-pic.avif" />
+		<source type="image/webp" srcset="assets/profile-pic.webp" />
+		<img
+			alt="Daniel Salvado"
+			class="aspect-square h-24 w-24 rounded-full object-cover"
+			loading="lazy"
+			src="assets/profile-pic.jpg"
+			width="96"
+			height="96"
+		/>
+	</picture>
+	<p>
+		Hi, my name is Daniel, I'm an enthusiastic developer who loves to work within the JavaScript ecosystem and I'm
+		always strive to improve on code and build beautiful things.
+	</p>
+	<p>
+		I love to share my know-how and things that I find interesting and because of that I do some writing on things that
+		I've tested and in some way, helped me on the products that I was building!
+	</p>
+	<p>Feel free to share the articles or even to contact me if you have any questions, suggestions or simply say hi!</p>
 </main>
