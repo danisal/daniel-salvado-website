7d016ffbc05fb071830dcd6f1c1f8b51e0d135bb
danisal <daniel.salvado@gmail.com>
2023-05-14 14:58:53 +0100

    Add work page

---
+++ src/routes/work/+page.svelte
@@ -0,0 +1,136 @@
+<svelte:head>
+	<title>Work | Daniel Salvado</title>
+</svelte:head>
+
+<main class="container prose mx-auto px-7 py-16">
+	<h1 class="text-gray-700">Work Experience</h1>
+	<section class="mt-12">
+		<div class="flex items-center justify-between">
+			<h2 class="m-0">
+				<a class="no-underline" href="https://www.crystallize.com" target="__blank" rel="noopener noreferrer"
+					>Crystallize</a
+				>
+			</h2>
+			<small><time datetime="2019-05">May 2019</time> - <time datetime="2023-04">April 2023</time></small>
+		</div>
+		<h4>Release Manager</h4>
+		<p>Schedule, manage, and coordinate releases across multiple applications.</p>
+		<p>Construct a release calendar for the different projects to have a centralized view of all releases.</p>
+		<p>Manage and mitigate risks and resolve issues regarding release quality and schedule.</p>
+		<p>Continuously monitor projects and provide reports about their progress.</p>
+		<p>Ensure all team members adhere to engineering best practices and enforce DevOps policies.</p>
+		<p>Monitor the release process and collect feedback from the different teams and customers for review.</p>
+		<p>Make improvements regularly to the release process.</p>
+		<em class="text-xs">GitHub · DevOps · Test Automation · Release Management</em>
+		<h4>Frontend Engineer</h4>
+		<p>Development of Crystallize Product Information Management SaaS.</p>
+		<p>
+			Improve the codebase, implement new features according to the current state of the art in software development and
+			good practices, and monitor application errors and exceptions with Sentry.
+		</p>
+		<p>Take part in feature and roadmap planning.</p>
+		<p>CI/CD company's main product and some other side projects, like conferences and company websites.</p>
+		<em class="text-xs"
+			>GitHub · React.js · GraphQL · TypeScript · Test Automation · HTML · CSS · Git · End-to-end Testing · Agile
+			Methodologies · Testing · Web Applications · Node.js · Front-End Development</em
+		>
+	</section>
+
+	<section class="mt-12">
+		<div class="flex items-center justify-between">
+			<h2 class="m-0">
+				<a class="no-underline" href="https://www.evodeck.com" target="__blank" rel="noopener noreferrer">Evodeck</a>
+			</h2>
+			<small><time datetime="2018-01">Jan 2018</time> - <time datetime="2019-04">Apr 2019</time></small>
+		</div>
+		<h4>Software Engineer</h4>
+		<p>Development of a garage management system application using ReactJS.</p>
+		<p>
+			Improve the codebase by introducing new ES6/ES7 features, Higher-Order Components (HOC), and applying DRY
+			principles.
+		</p>
+		<p>
+			As a member of an agile team, participate in the analyses and discussion of the project's technical constraints.
+		</p>
+		<p>Help develop another application using GraphQL, ReactJS, MongoDB, ExpressJS, Kubernetes, and Docker.</p>
+		<em class="text-xs"
+			>GitHub · React.js · HTML · CSS · Git · Agile Methodologies · Testing · Web Applications · Redux.js · Node.js ·
+			MongoDB · Front-End Development</em
+		>
+	</section>
+
+	<section class="mt-12">
+		<div class="flex items-center justify-between">
+			<h2 class="m-0">
+				<a class="no-underline" href="https://www.truphone.com" target="__blank" rel="noopener noreferrer">Truphone</a>
+			</h2>
+			<span><time datetime="2017-06">Jun 2017</time> - <time datetime="2017-12">Dec 2017</time></span>
+		</div>
+		<h4>Frontend Engineer</h4>
+		<p>Facilitate new payment options in web applications through seamless integration.</p>
+		<p>Optimize web styling by introducing the SASS pre-processor.</p>
+		<p>Jumpstart mobile app development with React Native framework</p>
+		<em class="text-xs"
+			>GitHub · HTML · CSS · Git · PugJS · Selenium WebDriver · Testing · Web Applications · React Native · Node.js ·
+			Front-End Development</em
+		>
+	</section>
+
+	<section class="mt-12">
+		<div class="flex items-center justify-between">
+			<h2 class="m-0">
+				<a class="no-underline" href="https://tradingeconomics.com" target="__blank" rel="noopener noreferrer"
+					>Trading Economics</a
+				>
+			</h2>
+			<small><time datetime="2016-10">Oct 2016</time> - <time datetime="2017-05">May 2017</time></small>
+		</div>
+		<h4>Software Developer</h4>
+		<p>
+			Successfully integrate PayPal payments and Twilio SMS notifications to create an advanced notification system for
+			financial indicators.
+		</p>
+		<p>
+			Refreshed the UI with Bootstrap 3 integration, enhancing the user experience of a high-traffic AngularJS
+			application.
+		</p>
+		<p>
+			Developed a management interface allowing admins to monitor users' notifications and subscription status for key
+			financial indicators.
+		</p>
+		<em class="text-xs"
+			>GitHub · JavaScript · HTML · CSS · Git · AngularJS · Node.js · MongoDB · Front-End Development</em
+		>
+	</section>
+
+	<section class="mt-12">
+		<div class="flex items-center justify-between">
+			<h2 class="m-0">
+				<a class="cursor-help no-underline" href="https://www.inem.pt" target="__blank" rel="noopener noreferrer"
+					><abbr title="Instituto Nacional de Emergência Médica">INEM</abbr></a
+				>
+			</h2>
+			<small><time datetime="2008-02">Feb 2008</time> - <time datetime="2016-09">Sep 2016</time></small>
+		</div>
+		<h4>Emergency Medical technician</h4>
+		<p>
+			Intervention in pre-hospital emergencies, aimed at triage, stabilisation and transport of the patient to health
+			facilities.
+		</p>
+		<p>Operate information and telecommunication systems.</p>
+		<p>Know how to act in situations of exception/disaster.</p>
+		<p>Driving emergency vehicles</p>
+		<em class="text-xs"
+			>EMT · Emergency Medical Services (EMS) · Automated External Defibrillator (AED) · Public Health</em
+		>
+	</section>
+	<p>
+		Need to know more about my working experience? You can have a better overview of it on my
+		<a
+			href="https://www.linkedin.com/in/daniel-salvado/"
+			rel="noopener noreferrer"
+			target="_blank"
+			aria-label="Daniel on linkedin">linkedIn</a
+		> or ping me through my social media profiles.
+	</p>
+</main>
