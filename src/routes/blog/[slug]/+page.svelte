eb5f1e65fd1136ba926e67e417e191935a2e6f7e
danisal <daniel.salvado@gmail.com>
2023-05-15 14:41:18 +0100

    Fix mobile responsiveness

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -4,7 +4,7 @@
 	const { title, date, content } = data;
 </script>
 
-<article class="prose mx-auto dark:prose-invert">
+<article class="container prose mx-auto px-7 py-16 dark:prose-invert">
 	<h1>{title}</h1>
 	<p>Published: {date}</p>
 	{@html content.html}
