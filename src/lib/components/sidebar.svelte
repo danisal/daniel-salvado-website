29cc733c41968d3777bd68f70bf3f98947fd53d1
danisal <daniel.salvado@gmail.com>
2023-12-19 21:26:07 +0000

    fix: sidebar dark mode

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <aside
-	class="fixed z-20 flex h-screen w-screen flex-col overscroll-none bg-white/95 px-2 pb-2 pt-20 backdrop-blur-lg transition-all duration-300 ease-in-out [@supports(backdrop-filter:blur(0px))]:bg-white/5"
+	class="fixed z-20 flex h-screen w-screen flex-col overscroll-none bg-white/95 px-2 pb-2 pt-20 backdrop-blur-lg transition-all duration-300 ease-in-out dark:bg-stone-950 dark:text-blue-200 [@supports(backdrop-filter:blur(0px))]:bg-white/5"
 	class:left-0={open}
 	class:-left-full={!open}
 >
