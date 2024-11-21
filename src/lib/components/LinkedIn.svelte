e48afcb6ea0f2ea9897db7d1127f75b57eb82556
danisal <daniel.salvado@gmail.com>
2024-11-21 18:25:06 +0000

    chore: refactor footer icons to use separate components
    - Replaced inline SVG icons with dedicated Svelte components for Twitter, GitHub, LinkedIn, and Bluesky.
    - This change enhances maintainability and simplifies future updates to these icons.

---
+++ src/lib/components/LinkedIn.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const props = $props();
+</script>
+
+<!--iconoir-->
+<svg {...props} stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentcolor"><path d="M21 8V16C21 18.7614 18.7614 21 16 21H8C5.23858 21 3 18.7614 3 16V8C3 5.23858 5.23858 3 8 3H16C18.7614 3 21 5.23858 21 8Z" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7 17V13.5V10" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M11 17V13.75M11 10V13.75M11 13.75C11 10 17 10 17 13.75V17" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7 7.01L7.01 6.99889" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+
+<!--svgl.app-->
+<!--<svg {...props} xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 256"><path d="M218.123 218.127h-37.931v-59.403c0-14.165-.253-32.4-19.728-32.4-19.756 0-22.779 15.434-22.779 31.369v60.43h-37.93V95.967h36.413v16.694h.51a39.907 39.907 0 0 1 35.928-19.733c38.445 0 45.533 25.288 45.533 58.186l-.016 67.013ZM56.955 79.27c-12.157.002-22.014-9.852-22.016-22.009-.002-12.157 9.851-22.014 22.008-22.016 12.157-.003 22.014 9.851 22.016 22.008A22.013 22.013 0 0 1 56.955 79.27m18.966 138.858H37.95V95.967h37.97v122.16ZM237.033.018H18.89C8.58-.098.125 8.161-.001 18.471v219.053c.122 10.315 8.576 18.582 18.89 18.474h218.144c10.336.128 18.823-8.139 18.966-18.474V18.454c-.147-10.33-8.635-18.588-18.966-18.453" fill="#0A66C2"/></svg>-->
+
+
+
--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,4 +1,9 @@
 <script>
+	import Twitter from '$lib/components/Twitter.svelte';
+	import Bluesky from '$lib/components/Bluesky.svelte';
+	import GitHub from '$lib/components/GitHub.svelte';
+	import LinkedIn from '$lib/components/LinkedIn.svelte';
+
 	const year = new Date().getFullYear();
 </script>
 
@@ -6,83 +11,28 @@
 	<p>&copy; {year} Daniel Salvado</p>
 	<div class="flex space-x-4">
 		<a
+			aria-label="Daniel on linkedin"
 			href="https://www.linkedin.com/in/daniel-salvado/"
 			rel="noopener noreferrer"
 			target="_blank"
-			aria-label="Daniel on linkedin"
 		>
-			<svg
-				class="h-6 w-6"
-				width="24px"
-				height="24px"
-				stroke-width="1.5"
-				viewBox="0 0 24 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				color="currentColor"
-				aria-label="github"
-				aria-hidden="true"
-				><path
-					d="M21 8v8a5 5 0 01-5 5H8a5 5 0 01-5-5V8a5 5 0 015-5h8a5 5 0 015 5zM7 17v-7"
-					stroke="currentColor"
-					stroke-width="1.5"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/><path
-					d="M11 17v-3.25M11 10v3.75m0 0c0-3.75 6-3.75 6 0V17M7 7.01l.01-.011"
-					stroke="currentColor"
-					stroke-width="1.5"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/></svg
-			>
+			<LinkedIn aria-hidden="true" aria-label="linkedin" class="h-6 w-6" />
 		</a>
-		<a href="https://github.com/danisal" rel="noopener noreferrer" target="_blank" aria-label="Daniel on github">
-			<svg
-				class="h-6 w-6"
-				aria-label="github"
-				aria-hidden="true"
-				width="24px"
-				height="24px"
-				stroke-width="1.5"
-				viewBox="0 0 24 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				color="currentColor"
-				><path
-					d="M16 22.027v-2.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 00-1.5-3.75 5.07 5.07 0 00-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 00-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 005 5.797a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 00-.94 2.58v2.87M9 20.027c-3 .973-5.5 0-7-3"
-					stroke="currentColor"
-					stroke-width="1.5"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/></svg
-			>
+		<a aria-label="Daniel on github" href="https://github.com/danisal" rel="noopener noreferrer" target="_blank">
+			<GitHub aria-hidden="true" aria-label="github" class="h-6 w-6" color="currentcolor"/>
 		</a>
 		<a
+			aria-label="Daniel on twitter"
 			href="https://twitter.com/danisalTweets"
 			rel="noopener noreferrer"
 			target="_blank"
-			aria-label="Daniel on twitter"
 		>
-			<svg
-				class="h-6 w-6"
-				width="24px"
-				height="24px"
-				stroke-width="1.5"
-				aria-label="twitter"
-				aria-hidden="true"
-				viewBox="0 0 24 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				><path
-					d="M23 3.01s-2.018 1.192-3.14 1.53a4.48 4.48 0 00-7.86 3v1a10.66 10.66 0 01-9-4.53s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5 0-.278-.028-.556-.08-.83C21.94 5.674 23 3.01 23 3.01z"
-					stroke="currentColor"
-					stroke-width="1.5"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/></svg
-			>
+			<Twitter aria-hidden="true" aria-label="twitter" class="h-6 w-6" color="currentColor"/>
 		</a>
+<!--		<a aria-label="Daniel on Bluesky" href="https://bsky.app/profile/danisal.bsky.social" rel="noopener noreferrer"-->
+<!--			 target="_blank">-->
+<!--			<Bluesky aria-hidden="true" aria-label="bluesky" class="h-6 w-6" />-->
+<!--		</a>-->
 	</div>
 	<a href="/legal-notice">Legal Notice</a>
 </footer>
